2765e222d128649678dd28fd8436758ac603b198
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2025-11-18 00:27:52 +0200

    Move header markup into Home component and update assets
    The header component was removed and its markup was moved directly into the Home component. Updated index.html to use a new favicon and title. Added new image assets for travel destinations.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,11 @@
 import { Route, Routes } from "react-router";
 import Home from "./components/home/Home.jsx";
 import DetailsPage from "./components/details-page/DetailsPage.jsx";
-import Header from "./components/header/Header.jsx";
-
-
-
-
 
 function App() {
   return (
     <>
-      <Header />
+
       <Routes>
         < Route path="/" element={<Home />} />
         < Route path="/details/:id/details" element={<DetailsPage />} />

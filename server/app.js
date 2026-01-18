210c8bb04e6acd64501554fe7fb802116efe4df7
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2026-01-19 01:44:26 +0200

    fix: reorder middleware and configure CORS for Vercel frontend

--- server/app.js
+++ server/app.js
@@ -6,10 +6,21 @@ import { errorHandler } from './middlewares/errorMiddleware.js';
 
 const app = express();
 
-app.use(cors());
+app.get('/health', (req, res) => res.status(200).send('ok'));
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',            
+    'http://localhost:3000',            
+    'https://travel-spots-jade.vercel.app/'  
+  ],
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
-app.use(routes);        
-app.use(errorHandler); 
+app.use(routes);
+app.use(errorHandler);
 
 export default app;

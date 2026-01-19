cc1249c54aeb89f9cda7f3a8cbe90888778011f5
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2026-01-20 00:12:24 +0200

    revert back vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3030', // your Express server port
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
